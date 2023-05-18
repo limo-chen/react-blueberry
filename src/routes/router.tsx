b40f957359afb8ce27686225277aa403a4f3e26e
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-18 11:35:54 +0800

    创建 ItemsPage 页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Root } from '../components/Root'
 import { WelcomeLayout } from '../layouts/WelcomeLayout'
 import { Home } from '../pages/Home'
+import { ItemsPage } from '../pages/ItemsPage'
 import { Welcome1 } from '../pages/Welcome1'
 import { Welcome2 } from '../pages/Welcome2'
 import { Welcome3 } from '../pages/Welcome3'
@@ -19,5 +20,5 @@ export const router = createBrowserRouter([
       { path: '4', element: <Welcome4 /> },
     ]
   },
-  { path: '/items', element: <div>items</div> },
+  { path: '/items', element: <ItemsPage /> },
 ])
