70f02916eb5107bd2c51b35ce93c46accaeb4997
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-18 18:34:38 +0800

    可以请求所有页数的数据了

--- mock/items.mock.ts
+++ mock/items.mock.ts
@@ -1,4 +1,5 @@
 import type { MockMethod } from 'vite-plugin-mock'
+import { faker } from '@faker-js/faker'
 
 let id = 0
 const createId = () => {
@@ -9,11 +10,11 @@ const create = (attrs?: Partial<Item>): Item => {
   return {
     id: createId(),
     user_id: 1,
-    amount: 1000,
+    amount: faker.datatype.number({ min: 99, max: 1000_00 }),
     tag_ids: [1, 2],
-    happen_at: '2021-08-01T00:00:00.000Z',
-    created_at: '2021-08-01T00:00:00.000Z',
-    updated_at: '2021-08-01T00:00:00.000Z',
+    happen_at: faker.date.past().toISOString(),
+    created_at: faker.date.past().toISOString(),
+    updated_at: faker.date.past().toISOString(),
     kind: 'expenses',
     ...attrs
   }
@@ -24,8 +25,10 @@ const createList = (n: number, attrs?: Partial<Item>): Item[] => {
 }
 
 const createResponse = ({ count = 10, perPage = 10, page = 1 }, attrs?: Partial<Item>,): Resources<Item> => {
-  return {
-    resources: createList(perPage, attrs),
+    const sendCount = (page - 1) * perPage
+    const left = count - sendCount
+    return {
+        resources: left > 0 ? createList(Math.min(left, perPage), attrs) : [],
     pager: {
       page,
       per_page: perPage,
@@ -39,6 +42,6 @@ export const itemsMock: MockMethod = {
   method: 'get',
   statusCode: 200,
   response: ({ query }: ResponseParams): Resources<Item> =>
-    createResponse({ count: 100, perPage: 10, page: parseInt(query.page) })
+  createResponse({ count: 30, perPage: 10, page: parseInt(query.page) })
   ,
 }
