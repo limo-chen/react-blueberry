03e37fdc4078a9b4e4b747a7766f5e8111620f9d
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-18 09:28:30 +0800

    }))

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Uncos from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     Uncos(),
-    react(),
+    react(),  
+    viteMockServe()
   ],
-})
+}))
