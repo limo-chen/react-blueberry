4d183ee9ec927e506fd848ac2ac5fad2d9a3ccf9
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-18 20:07:17 +0800

    完成登录页面样式

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig(({ command }) => ({
     Uncos(),
     react(),  
     viteMockServe(),
-    svgsprites({ noOptimizeList: ['chart', 'category', 'export', 'noty'] })
+    svgsprites({ noOptimizeList: ['logo', 'chart', 'category', 'export', 'noty'] })
   ],
 }))
