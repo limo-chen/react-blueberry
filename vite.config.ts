bd051362178d588a9b61e624e878ff79f7409802
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-21 13:19:10 +0800

    完成数字面板的样式

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig(({ command }) => ({
     Uncos(),
     react(),  
     viteMockServe(),
-    svgsprites({ noOptimizeList: ['logo', 'chart', 'category', 'export', 'noty'] })
+    svgsprites({ noOptimizeList: ['logo', 'chart', 'category', 'export', 'noty', 'calendar'] })
   ],
 }))
