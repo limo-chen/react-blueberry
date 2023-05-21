620c4af7e89d4645dca257659b46b83ad395c463
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-21 17:17:47 +0800

    添加 Select 选项

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -8,6 +8,7 @@ import { TopNav } from '../components/TopNav'
 import { LineChart } from '../components/LineChart'
 import { PieChart } from '../components/PieChart'
 import { RankChart } from '../components/RankChart'
+import { Input } from '../components/Input'
 
 
 export const StatisticsPage: React.FC = () => {
@@ -47,13 +48,14 @@ export const StatisticsPage: React.FC = () => {
   const items2 = [
     { tag: { name: '吃饭', sign: '😨' }, amount: 10000 },
     { tag: { name: '打车', sign: '🥱' }, amount: 20000 },
-    { tag: { name: '买皮肤', sign: '💖' }, amount: 68800 },
+    { tag: { name: '敲代码', sign: '💖' }, amount: 68800 },
   ].map(item => ({ x: item.tag.name, y: item.amount / 100 }))
   const items3 = [
     { tag: { name: '吃饭', sign: '😨' }, amount: 10000 },
     { tag: { name: '打车', sign: '🥱' }, amount: 20000 },
-    { tag: { name: '买皮肤', sign: '💖' }, amount: 68800 },
+    { tag: { name: '敲代码', sign: '💖' }, amount: 68800 },
   ].map(item => ({ name: item.tag.name, value: item.amount, sign: item.tag.sign }))
+  const [x, setX] = useState('expenses')
   return (
     <div>
       <Gradient>
@@ -62,6 +64,15 @@ export const StatisticsPage: React.FC = () => {
         } />
       </Gradient>
       <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
+      <div flex p-16px items-center gap-x-16px>
+        <span grow-0 shrink-0>类型</span>
+        <div grow-1 shrink-1>
+          <Input type="select" options={[
+            { text: '支出', value: 'expenses' },
+            { text: '收入', value: 'income' },
+          ]} value={x} onChange={value => setX(value)} disableError/>
+        </div>
+      </div>
       <LineChart className="h-120px" items={items} />
       <PieChart className="h-260px m-t-16px" items={items2} />
       <RankChart className="m-t-8px" items={items3} />
