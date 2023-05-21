76ec77a20288589f5e2f1e10818860d817e00fe5
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-21 15:27:45 +0800

    创建 新建标签页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -9,6 +9,7 @@ import { Welcome1 } from '../pages/Welcome1'
 import { Welcome2 } from '../pages/Welcome2'
 import { Welcome3 } from '../pages/Welcome3'
 import { Welcome4 } from '../pages/Welcome4'
+import { TagsNewPage } from '../pages/TagsNewPage'
 export const router = createBrowserRouter([
   { path: '/', element: <Root />, },
   { path: '/home', element: <Home title="首页" /> },
@@ -20,10 +21,12 @@ export const router = createBrowserRouter([
       { path: '2', element: <Welcome2 /> },
       { path: '3', element: <Welcome3 /> },
       { path: '4', element: <Welcome4 /> },
+
     ]
   },
   { path: '/items', element: <ItemsPage /> },
   { path: '/items/new', element: <ItemsNewPage /> },
+  { path: '/tags/new', element: <TagsNewPage /> },
   { path: '/sign_in', element: <SignInPage /> },
   { path: '/chart', element: <div>图表</div> },
   { path: '/export', element: <div>敬请期待</div> },
