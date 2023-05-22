1182daa9c53dbbebb80efd2e564639d9878308ed
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 10:28:53 +0800

    优化倒计时细节

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -16,28 +16,24 @@ type Props = {
       await request()
       setStarted(new Date())
     }
-    useEffect(() => {
-        if (started) {
-            timer.current = window.setInterval(() => {
-              const t = new Date()
-              const seconds = Math.round((t.getTime() - started.getTime()) / 1000)
-              if (maxCount - seconds < 0) {
-                setStarted(undefined)
-              }
-              setCount(maxCount - seconds)
-            }, 1000)
-          } else {
-            if (timer.current) {
-              window.clearInterval(timer.current)
-              timer.current = undefined
-            }
+    const clearTimer = () => {
+        if (timer.current) {
+          window.clearInterval(timer.current)
+          timer.current = undefined
           }
-          return () => {
-            if (timer.current) {
-              window.clearInterval(timer.current)
-              timer.current = undefined
-            }
+        }
+        useEffect(() => {
+          if (!started) {
+            clearTimer()
+            return
       }
+      timer.current = window.setInterval(() => {
+        const seconds = Math.round((new Date().getTime() - started.getTime()) / 1000)
+        const count = maxCount - seconds
+        if (count < 0) { setStarted(undefined) }
+        setCount(count)
+      }, 1000)
+      return clearTimer
     }, [started])
   
     return (
@@ -45,7 +41,9 @@ type Props = {
         <input shrink-1 j-input-text type="text" placeholder={placeholder} max-w="[calc(40%-8px)]"
           value={value} onChange={e => onChange?.(e.target.value)} />
         {started
-         ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn disabled text-gray>{count}</button>
+         ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn disabled text-gray>
+         {count}秒后可重发
+       </button>
         : <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn onClick={onClick}>
           发送验证码
         </button>
