69919787e1d73cde39108a97595840fc1c7d48a9
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 11:17:39 +0800

    使用 useLoadingStore 来控制全局加载器的显示和隐藏

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,11 +1,11 @@
 import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
+import { useLoadingStore } from '../stores/useLoadingStore'
 
 // 静态配置项直接用 defaults 配置
 axios.defaults.baseURL = isDev ? '/' : 'http://121.196.236.94:8080/api/v1'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.timeout = 10000
-
 // 动态配置项用拦截器来配置
 axios.interceptors.request.use((config) => {
   const jwt = localStorage.getItem('jwt') || ''
@@ -13,7 +13,6 @@ axios.interceptors.request.use((config) => {
   if (jwt) { config.headers.Authorization = `Bearer ${jwt}` }
   return config
 })
-
 // 封装 axios
 export const ajax = {
   get: <T>(path: string, config?: AxiosRequestConfig<any>) => {
@@ -24,4 +23,26 @@ export const ajax = {
   },
   patch: () => { },
   delete: () => { },
+}
+
+type Options = {
+  showLoading?: boolean
+}
+export const useAjax = (options?: Options) => {
+  const showLoading = options?.showLoading || false
+  const { setVisible } = useLoadingStore()
+  const ajax = {
+    get: <T>(path: string, config?: AxiosRequestConfig<any>) => {
+      return axios.get<T>(path, config)
+    },
+    post: <T>(path: string, data: JSONValue) => {
+      if (showLoading) { setVisible(true) }
+      return axios.post<T>(path, data).finally(() => {
+        if (showLoading) { setVisible(false) }
+      })
+    },
+    patch: () => { },
+    delete: () => { },
+  }
+  return ajax
 }
