d880f6e33b978a4d41263cd22d998c3a6dac9acc
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 15:29:25 +0800

    上个月可以点击获取数据啦

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -16,20 +16,32 @@ import { time } from '../lib/time'
 type Groups = { happen_at: string; amount: number }[]
 type Groups2 = { tag_id: number; tag: Tag; amount: number }[]
 const format = 'yyyy-MM-dd'
-export const StatisticsPage: React.FC = () => {
-  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
-  const [kind, setKind] = useState('expenses')
-  const { get } = useAjax({ showLoading: false, handleError: true })
-
-  const generateStartEnd = () => {
-    if (timeRange === 'thisMonth') {
-        const start = time().firstDayOfMonth
-        const end = time().lastDayOfMonth.add(1, 'day')
-        return { start, end }
-    } else {
-        return { start: time(), end: time() }
-    }
+type GetKeyParams = {
+    start: Time
+    end: Time
+    kind: Item['kind']
+    group_by: 'happen_at' | 'tag_id'
   }
+  const getKey = ({ start, end, kind, group_by }: GetKeyParams) => {
+    return `/api/v1/items/summary?happened_after=${start.format('yyyy-MM-dd')}&happened_before=${end.format('yyyy-MM-dd')}&kind=${kind}&group_by=${group_by}`
+  }
+  export const StatisticsPage: React.FC = () => {
+    const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+    const [kind, setKind] = useState<Item['kind']>('expenses')
+    const { get } = useAjax({ showLoading: false, handleError: true })
+  
+    const generateStartEnd = () => {
+      let start: Time
+      if (timeRange === 'thisMonth') {
+        start = time().firstDayOfMonth
+      } else if (timeRange === 'lastMonth') {
+        start = time().add(-1, 'month').firstDayOfMonth
+      } else {
+        start = time()
+      }
+      const end = start.lastDayOfMonth.add(1, 'day')
+      return { start, end }
+    }
   const generateDefaultItems = (time: Time) => {
     return Array.from({ length: start.dayCountOfMonth }).map((_, i) => {
       const x = start.clone.add(i, 'day').format(format)
@@ -38,7 +50,7 @@ export const StatisticsPage: React.FC = () => {
   }
   const { start, end } = generateStartEnd()
   const defaultItems = generateDefaultItems(start)
-  const { data: items } = useSWR(`/api/v1/items/summary?happened_after=${start}&happened_before=${end}&kind=${kind}&group_by=happen_at`,
+  const { data: items } = useSWR(getKey({ start, end, kind, group_by: 'happen_at' }),
     async (path) =>
       (await get<{ groups: Groups; total: number }>(path)).data.groups
       .map(({ happen_at, amount }) => ({ x: happen_at, y: (amount / 100).toFixed(2) }))
@@ -46,14 +58,14 @@ export const StatisticsPage: React.FC = () => {
   const normalizedItems = defaultItems?.map((defaultItem, index) =>
   items?.find((item) => item.x === defaultItem.x) || defaultItem
 )
-const { data: items2 } = useSWR(`/api/v1/items/summary?happened_after=${start}&happened_before=${end}&kind=${kind}&group_by=tag_id`,
+const { data: items2 } = useSWR(getKey({ start, end, kind, group_by: 'tag_id' }),
 async (path) =>
   (await get<{ groups: Groups2; total: number }>(path)).data.groups
     .map(({ tag_id, tag, amount }) =>
       ({ name: tag.name, value: (amount / 100).toFixed(2), sign: tag.sign }))
 )
 
-  return (
+return (
     <div>
       <Gradient>
         <TopNav title="统计图表" icon={
@@ -73,7 +85,7 @@ async (path) =>
           <Input type="select" options={[
             { text: '支出', value: 'expenses' },
             { text: '收入', value: 'income' },
-        ]} value={kind} onChange={value => setKind(value)} disableError />
+          ]} value={kind} onChange={value => setKind(value)} disableError />
         </div>
       </div>
       <LineChart className="h-120px" items={normalizedItems} />
