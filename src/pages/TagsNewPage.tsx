23f91d8597ab2212ea9fdd03efb70e7489241560
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 14:19:41 +0800

    封装 BackIcon 按钮，方便后退

--- src/pages/TagsNewPage.tsx
+++ src/pages/TagsNewPage.tsx
@@ -1,6 +1,6 @@
 import { Gradient } from '../components/Gradient'
 import { TopNav } from '../components/TopNav'
-import { Icon } from '../components/Icon'
+import { BackIcon } from '../components/BackIcon'
 import { TagForm } from './TagsNewPage/TagForm'
 
 export const TagsNewPage: React.FC = () => {
@@ -8,7 +8,7 @@ export const TagsNewPage: React.FC = () => {
   return (
     <div>
     <Gradient className="grow-0 shrink-0">
-        <TopNav title="新建标签" icon={<Icon name="back" />} />
+        <TopNav title="新建标签" icon={<BackIcon name="back" />} />
       </Gradient>
       <TagForm type="create" />
     </div>
