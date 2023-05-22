90a60b0e2bb7745c462292dfccb7c9be524888b8
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 11:24:01 +0800

    删除 ajax 导出，只使用 useAjax

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import useSWRInfinite from 'swr/infinite'
-import { ajax } from '../../lib/ajax'
+import { useAjax } from '../../lib/ajax'
 interface Props {}
 const Div = styled.div`
   padding: 16px;
@@ -16,9 +16,10 @@ const getKey = (pageIndex: number, prev: Resources<Item>) => {
       }
      
       export const ItemsList: React.FC<Props> = () => {
+        const { get } = useAjax()
         const { data, error, size, setSize } = useSWRInfinite(
             getKey,
-            async path => (await ajax.get<Resources<Item>>(path)).data,
+            async path => (await get<Resources<Item>>(path)).data,
             { revalidateFirstPage: false }
         )
         const onLoadMore = () => {
