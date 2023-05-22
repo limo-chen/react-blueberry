eb72eb18c0900d530a791bd0f6f78a4f73d88cbe
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 15:26:33 +0800

    三个图表都从服务器请求数据

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -52,11 +52,7 @@ async (path) =>
     .map(({ tag_id, tag, amount }) =>
       ({ name: tag.name, value: (amount / 100).toFixed(2), sign: tag.sign }))
 )
-  const items3 = [
-    { tag: { name: '吃饭', sign: '😨' }, amount: 10000 },
-    { tag: { name: '打车', sign: '🥱' }, amount: 20000 },
-    { tag: { name: '敲代码', sign: '💖' }, amount: 68800 },
-  ].map(item => ({ name: item.tag.name, value: item.amount, sign: item.tag.sign }))
+
   return (
     <div>
       <Gradient>
