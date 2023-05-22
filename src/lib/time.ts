d78f176c27c8ebd87a7ccffe6abe24a312a66a56
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 15:03:50 +0800

    items 不再是写死的，而是从后端获取的

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -22,6 +22,9 @@ type Parts = {
     get lastDayOfMonth() {
         return new Time(new Date(this.year, this.month - 1 + 1, 0))
       }
+      get firstDayOfMonth() {
+        return new Time(new Date(this.year, this.month - 1, 1))
+      }
     /**
      * 格式化输出
      * @param pattern 目前只支持 yyyy MM dd HH mm ss fff
