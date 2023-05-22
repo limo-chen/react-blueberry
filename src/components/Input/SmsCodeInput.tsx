ff098eb12a5d766715e603a0b9b59e84f61299c1
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 10:18:29 +0800

    将 onClick 重命名为 request

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -2,11 +2,15 @@ type Props = {
     value?: string
     placeholder?: string
     onChange?: (value: string) => void
-    onClick?: () => void
+    request?: () => Promise<unknown>
   }
   export const SmsCodeInput: React.FC<Props> = (props) => {
-    const { value, placeholder, onChange, onClick } = props
-    return (
+    const { value, placeholder, onChange, request } = props
+    const onClick = async () => {
+      if (!request) { return }
+      await request()
+      // 开始倒计时
+    }
       <div flex gap-x-16px>
         <input shrink-1 j-input-text type="text" placeholder={placeholder} max-w="[calc(40%-8px)]"
           value={value} onChange={e => onChange?.(e.target.value)} />
