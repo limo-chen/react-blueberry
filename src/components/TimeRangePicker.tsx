e560fee4976d9d92e17858c940f290c7af2b34ce
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-22 15:40:49 +0800

    点击自定义时间时会弹框，遇到了 bug

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -1,3 +1,4 @@
+import { usePopup } from '../hooks/usePopup'
 import { Tabs } from './Tabs'
 export type TimeRange =
   | 'thisMonth'
@@ -19,8 +20,26 @@ const defaultTimeRanges: { key: TimeRange; text: string }[] = [
     timeRanges?: { key: TimeRange; text: string }[]
   }
   export const TimeRangePicker: React.FC<Props> = (props) => {
-    const { selected, onSelect, timeRanges = defaultTimeRanges } = props
-    return (
-      <Tabs tabItems={timeRanges} value={selected} onChange={onSelect} />
-      )
+    const { selected, onSelect: _onSelect, timeRanges = defaultTimeRanges } = props
+    const onConfirm = () => {
+      _onSelect('custom')
     }
+    const { popup, hide, show } = usePopup({
+      children: <div onClick={onConfirm}>弹框</div>,
+      position: 'center'
+    })
+    const onSelect = (key: TimeRange) => {
+      if (key === 'custom') {
+        // 弹框
+        show()
+      } else {
+        _onSelect(key)
+      }
+    }
+    return (
+      <>
+        {popup}
+        <Tabs tabItems={timeRanges} value={selected} onChange={onSelect} />
+      </>
+    )
+  }
