9dffef2c34dd36fafb5277cc86eb6ff49c8d6bf9
陈京璐 <12379330+hanyi319@user.noreply.gitee.com>
2023-05-23 13:47:42 +0800

    修复一些bug

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -14,29 +14,26 @@ import type { Time } from '../lib/time'
 import { time } from '../lib/time'
 import { BackIcon } from '../components/BackIcon'
 
-
 type Groups = { happen_at: string; amount: number }[]
 type Groups2 = { tag_id: number; tag: Tag; amount: number }[]
 const format = 'yyyy-MM-dd'
 type GetKeyParams = {
-    start: Time
-    end: Time
-    kind: Item['kind']
-    group_by: 'happen_at' | 'tag_id'
-  }
-  const getKey = ({ start, end, kind, group_by }: GetKeyParams) => {
-    return `/api/v1/items/summary?happened_after=${start.format('yyyy-MM-dd')}&happened_before=${end.format('yyyy-MM-dd')}&kind=${kind}&group_by=${group_by}`
-  }
-
-  export const StatisticsPage: React.FC = () => {
-    const [timeRange, setTimeRange] = useState<TimeRange>({
-        name: 'thisMonth',
-        start: time().firstDayOfMonth,
-        end: time().lastDayOfMonth.add(1, 'day')
-      })
-    const [kind, setKind] = useState<Item['kind']>('expenses')
-    const { get } = useAjax({ showLoading: false, handleError: true })
-  
+  start: Time
+  end: Time
+  kind: Item['kind']
+  group_by: 'happen_at' | 'tag_id'
+}
+const getKey = ({ start, end, kind, group_by }: GetKeyParams) => {
+  return `/api/v1/items/summary?happened_after=${start.format('yyyy-MM-dd')}&happened_before=${end.format('yyyy-MM-dd')}&kind=${kind}&group_by=${group_by}`
+}
+export const StatisticsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>({
+    name: 'thisMonth',
+    start: time().firstDayOfMonth,
+    end: time().lastDayOfMonth.add(1, 'day')
+  })
+  const [kind, setKind] = useState<Item['kind']>('expenses')
+  const { get } = useAjax({ showLoading: false, handleError: true })
 
   const generateDefaultItems = (time: Time) => {
     return Array.from({ length: start.dayCountOfMonth }).map((_, i) => {
@@ -44,50 +41,48 @@ type GetKeyParams = {
       return { x, y: 0 }
     })
   }
- 
   const { start, end } = timeRange
   const defaultItems = generateDefaultItems(start)
   const { data: items } = useSWR(getKey({ start, end, kind, group_by: 'happen_at' }),
     async (path) =>
       (await get<{ groups: Groups; total: number }>(path)).data.groups
-      .map(({ happen_at, amount }) => ({ x: happen_at, y: (amount / 100).toFixed(2) }))
-      )
+        .map(({ happen_at, amount }) => ({ x: happen_at, y: (amount / 100).toFixed(2) }))
+  )
   const normalizedItems = defaultItems?.map((defaultItem, index) =>
-  items?.find((item) => item.x === defaultItem.x) || defaultItem
-)
-const { data: items2 } = useSWR(getKey({ start, end, kind, group_by: 'tag_id' }),
-async (path) =>
-  (await get<{ groups: Groups2; total: number }>(path)).data.groups
-    .map(({ tag_id, tag, amount }) =>
-      ({ name: tag.name, value: (amount / 100).toFixed(2), sign: tag.sign }))
-)
-
-return (
+    items?.find((item) => item.x === defaultItem.x) || defaultItem
+  )
+  const { data: items2 } = useSWR(getKey({ start, end, kind, group_by: 'tag_id' }),
+    async (path) =>
+      (await get<{ groups: Groups2; total: number }>(path)).data.groups
+        .map(({ tag_id, tag, amount }) =>
+          ({ name: tag.name, value: (amount / 100).toFixed(2), sign: tag.sign }))
+  )
+  return (
     <div>
       <Gradient>
         <TopNav title="统计图表" icon={
-     <BackIcon />
+          <BackIcon />
         } />
       </Gradient>
       <TimeRangePicker selected={timeRange} onSelect={setTimeRange}
         timeRanges={[
-            {
-                text: '本月',
-                key: { name: 'thisMonth', start: time().firstDayOfMonth, end: time().lastDayOfMonth.add(1, 'day') },
-              },
-              {
-                text: '上月',
-                key: { name: 'lastMonth', start: time().add(-1, 'month').firstDayOfMonth, end: time().add(-1, 'month').lastDayOfMonth.add(1, 'day') },
-              },
-              {
-                text: '两个月前',
-                key: { name: 'twoMonthsAgo', start: time().add(-2, 'month').firstDayOfMonth, end: time().add(-2, 'month').lastDayOfMonth.add(1, 'day') },
-              },
-              {
-                text: '三个月前',
-                key: { name: 'threeMonthsAgo', start: time().add(-3, 'month').firstDayOfMonth, end: time().add(-3, 'month').lastDayOfMonth.add(1, 'day') },
-            },        
-            ]} />
+          {
+            text: '本月',
+            key: { name: 'thisMonth', start: time().firstDayOfMonth, end: time().lastDayOfMonth.add(1, 'day') },
+          },
+          {
+            text: '上月',
+            key: { name: 'lastMonth', start: time().add(-1, 'month').firstDayOfMonth, end: time().add(-1, 'month').lastDayOfMonth.add(1, 'day') },
+          },
+          {
+            text: '两个月前',
+            key: { name: 'twoMonthsAgo', start: time().add(-2, 'month').firstDayOfMonth, end: time().add(-2, 'month').lastDayOfMonth.add(1, 'day') },
+          },
+          {
+            text: '三个月前',
+            key: { name: 'threeMonthsAgo', start: time().add(-3, 'month').firstDayOfMonth, end: time().add(-3, 'month').lastDayOfMonth.add(1, 'day') },
+          },
+        ]} />
       <div flex p-16px items-center gap-x-16px>
         <span grow-0 shrink-0>类型</span>
         <div grow-1 shrink-1>
